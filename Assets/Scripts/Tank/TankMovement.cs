12b9570405bfccc1638f36a87b95dd51842e6e2b
Pedro Mota <pedrlse3@gmail.com>
2022-03-03 16:42:47 +0000

    Tank development update
    Setup the audio of the tank.
    Setup forward and backwards driving movement.
    Setup smooth turning.

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -2,15 +2,15 @@
 
 public class TankMovement : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;         
-    public float m_Speed = 12f;            
-    public float m_TurnSpeed = 180f;       
-    public AudioSource m_MovementAudio;    
-    public AudioClip m_EngineIdling;       
-    public AudioClip m_EngineDriving;      
-    public float m_PitchRange = 0.2f;
+    public int m_PlayerNumber = 1;  //defines which tank we are talking about        
+    public float m_Speed = 12f; //defines the movement speed of the tank           
+    public float m_TurnSpeed = 180f;    //defines how fast the tank can turn
+    public AudioSource m_MovementAudio; //plays the audio in the scene   
+    public AudioClip m_EngineIdling;    //gets the audio to be reporduced by the audisource
+    public AudioClip m_EngineDriving;    
+    public float m_PitchRange = 0.2f;   //changes the current pitch   
 
-    /*
+    
     private string m_MovementAxisName;     
     private string m_TurnAxisName;         
     private Rigidbody m_Rigidbody;         
@@ -46,34 +46,67 @@ public class TankMovement : MonoBehaviour
 
         m_OriginalPitch = m_MovementAudio.pitch;
     }
-    */
+    
 
     private void Update()
     {
         // Store the player's input and make sure the audio for the engine is playing.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+        EngineAudio();  //play the engineaudio function
     }
 
 
     private void EngineAudio()
     {
         // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+
+        if(Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
+        {
+            if(m_MovementAudio.clip == m_EngineDriving)
+            {
+                m_MovementAudio.clip = m_EngineIdling; //switch the clip the audioclip is playing
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);   //set the pitch value to a random number between the values defined on each side of the comma
+                m_MovementAudio.Play (); //call the play function everytime the audio changes
+            }
+        }
+        else
+        {
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                m_MovementAudio.clip = m_EngineDriving; //switch the clip the audioclip is playing
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);   //set the pitch value to a random number between the values defined on each side of the comma
+                m_MovementAudio.Play(); //call the play function everytime the audio changes
+            }
+        }
     }
 
 
     private void FixedUpdate()
     {
         // Move and turn the tank.
+        Move();
+        Turn();
     }
 
 
     private void Move()
     {
         // Adjust the position of the tank based on the player's input.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);  //makes the tank move towards the direction of the movement
     }
 
 
     private void Turn()
     {
         // Adjust the rotation of the tank based on the player's input.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+
     }
 }
