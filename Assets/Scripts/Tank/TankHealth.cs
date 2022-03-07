e0462593f198b96502447aa59c01f62edda1af71
Pedro Mota <pedro.mota@exaud.com>
2022-03-07 10:30:04 +0000

    created health changer

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -3,14 +3,14 @@ using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
-    public float m_StartingHealth = 100f;          
-    public Slider m_Slider;                        
-    public Image m_FillImage;                      
+    public float m_StartingHealth = 100f;   //defines the value of the satrting health       
+    public Slider m_Slider; //used to reference the slider created in the scene                        
+    public Image m_FillImage;   //used to acess the image present inside the slider                    
     public Color m_FullHealthColor = Color.green;  
     public Color m_ZeroHealthColor = Color.red;    
     public GameObject m_ExplosionPrefab;
     
-    /*
+    
     private AudioSource m_ExplosionAudio;          
     private ParticleSystem m_ExplosionParticles;   
     private float m_CurrentHealth;  
@@ -19,12 +19,11 @@ public class TankHealth : MonoBehaviour
 
     private void Awake()
     {
-        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
-        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>(); //gets component from a original object "m_ExplosionPrefab" and clones the object
+        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>(); //gets the explosion audio from the audiosource 
 
-        m_ExplosionParticles.gameObject.SetActive(false);
+        m_ExplosionParticles.gameObject.SetActive(false); //turns down the explosion gameobject as it doesnt have to explode at the start
     }
-
 
     private void OnEnable()
     {
@@ -33,11 +32,13 @@ public class TankHealth : MonoBehaviour
 
         SetHealthUI();
     }
-    */
+    
 
     public void TakeDamage(float amount)
     {
         // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+        m_CurrentHealth -= amount; //reduces the health according to the ammount of damage
+        SetHealthUI();
     }
 
 
